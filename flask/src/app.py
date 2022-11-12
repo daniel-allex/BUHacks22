cb6e8b4efcd68fe51e34ab3030c5db7b6b4bc560
Daniel Allex <danny.allex@gmail.com>
2022-11-12 14:18:33 -0500

    search plugin working on backend

--- flask/src/app.py
+++ flask/src/app.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template, url_for, request, redirect, json
 from flask_cors import CORS
+from duckduckgo_search import ddg_news
+
 
 app = Flask(__name__)
 CORS(app)
@@ -13,19 +15,37 @@ def index():
 def handleRequest():
     print("requested")
     text = request.get_json()["contents"]
-    print(text)
     #se = text.split("\n")
     #response = json.dumps({"contents": se[0]})
     #response.headers.add('Access-Control-Allow-Origin', '*')
     #return response
+    
+    keywords = "Climate Change"
+
+    r = ddg_news(keywords, region='wt-wt', safesearch='Off', time='d', max_results=5)
+
     example = [{
+       "sentence": "Climate Change",
+        "results": [{
+            "error": "Interviews with dozens of officials showed the outside forces,",
+            "source": r[0]["url"],
+           "correct": r[0]["title"]
+        },
+        {
+            "error": "Interviews with dozens of officials showed the outside forces,",
+            "source": r[1]["url"],
+           "correct": r[1]["title"]
+        }]
+        },
+        {
        "sentence": "Showing results",
         "results": [{
-            "error": "He did not make an AI",
+            "error": "The upcoming heavy snows and freezing temperatures will be a major factor in the war, officials said.",
             "source": "https://www.politifact.com/factchecks/2020/jul/28/stella-immanuel/dont-fall-video-hydroxychloroquine-not-covid-19-cu/",
            "correct": "PolitiFact | Hydroxychloroquine is not a COVID-19 cure"
         }]
         }]
+        
 
     return json.dumps(example)
 
