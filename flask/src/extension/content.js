b2467dd76cd04f775aa5c574cd4b1e677c2fc03b
Daniel Allex <danny.allex@gmail.com>
2022-11-13 06:50:51 -0500

    updated extension

--- flask/src/extension/content.js
+++ flask/src/extension/content.js
@@ -1,32 +1,22 @@
 var sentenceQueue = [];
-var journalistQueue = [];
 var original = true;
 var totalWords = 0;
-var misinfoWords = 0;
-var politicalColors = {
-    "left": "#1565c0",
-    "left-center": "#42a5f5",
-    "center": "#ba68c8",
-    "right-center": "#e57373",
-    "right": "#e53935",
-}
-var mouseOverMisinformation = false;
-var mouseOverJournalist = false;
+var mouseOverHighlight = false;
 
-function highlightMisinfo(data) {
+function highlightRelevant(data) {
     var context = document.body; // requires an element with class "context" to exist
     var instance = new Mark(context);
     var i
     var options = {
         "accuracy": "exactly",
         "separateWordSearch": false,
-        "className": "MarkUpMisinformation",
-        "exclude": [".MarkUpMisinformation", ".misinfoBoxes", "#headingTab", ".blockTabs", ".blockTabInfo"],
+        "className": "MarkUp",
+        "exclude": [".MarkUp", ".newsBoxes", "#headingTab", ".blockTabs", ".blockTabInfo"],
         "each": function (node) {
             // node is the marked DOM element
             var box = document.createElement('div');
             let boxStyle = box.style;
-            box.className = "misinfoBoxes";
+            box.className = "newsBoxes";
 
             //boxStyle.top = coords.bottom + "px";
             createBoxHTML(box, data, i);
@@ -35,118 +25,42 @@ function highlightMisinfo(data) {
 
             //show box
             node.onmouseover = function (event) {
-                mouseOverMisinformation = true;
+                mouseOverHighlight = true;
 
-                if (mouseOverJournalist === false) {
-                    //let box = node.firstElementChild.style;
-                    let coords = getCoords(node);
-                    boxStyle.width = node.offsetWidth + "px";
-                    boxStyle.left = coords.left + "px";
-                    boxStyle.top = coords.bottom + "px";
-                    box.style.display = 'block'; //display box
-                    box.style.position = "absolute"
-                }
+                //let box = node.firstElementChild.style;
+                let coords = getCoords(node);
+                boxStyle.width = node.offsetWidth + "px";
+                boxStyle.left = coords.left + "px";
+                boxStyle.top = coords.bottom + "px";
+                box.style.display = 'block'; //display box
+                box.style.position = "absolute"
             };
 
             //hide box
             node.onmouseout = function (event) {
-                mouseOverMisinformation = false;
+                mouseOverHighlight = false;
                 //let box = node.firstElementChild.style;
                 box.style.display = 'none'; //hide box
 
             };
 
             box.onmouseenter = function (event) {
-                mouseOverMisinformation = true;
+                mouseOverHighlight = true;
                 box.style.display = 'block'; //display box
             };
 
             box.onmouseleave = function (event) {
-                mouseOverMisinformation = false;
+                mouseOverHighlight = false;
                 boxStyle.display = 'none'; //hide box
 
             };
         }
     };
     for (i = 0; i < data.length; i++) {
-        misinfoWords = misinfoWords + data[i].sentence.length;
         instance.mark(data[i].sentence, options);
     }
 }
 
-function highlightJournalists() {
-    var i
-    var context = document.body // requires an element with class "context" to exist
-    var instance = new Mark(context);
-    var options = {
-        "accuracy": {
-            "value": "exactly",
-            "limiters": [",", "."]
-        },
-        "exclude": [".MarkUpJournalists", ".misinfoBoxes", "#headingTab", ".blockTabs", ".blockTabInfo"],
-        "separateWordSearch": false,
-        "className": "MarkUpJournalists",
-        "caseSensitive": false,
-        "each": function (node) {
-            // node is the marked DOM element
-            node.style.zIndex = node.style.zIndex + 1;
-            var box = document.createElement('div');
-            let boxStyle = box.style;
-            box.className = "misinfoBoxes";
-
-            //boxStyle.top = coords.bottom + "px";
-            createBoxJournalists(node, box, i);
-
-            document.body.appendChild(box);
-
-            //show box
-            node.onmouseover = function (event) {
-                mouseOverJournalist = true;
-
-                if (mouseOverMisinformation === false) {
-                    //let box = node.firstElementChild.style;
-                    let coords = getCoords(node);
-                    boxStyle.width = node.offsetWidth + "px";
-                    boxStyle.left = coords.left + "px";
-                    boxStyle.top = coords.bottom + "px";
-                    box.style.display = 'block'; //display box
-                    box.style.position = "absolute"
-                }
-            };
-
-            //hide box
-            node.onmouseout = function (event) {
-                mouseOverJournalist = false;
-                //let box = node.firstElementChild.style;
-                box.style.display = 'none'; //hide box
-
-            };
-
-            box.onmouseenter = function (event) {
-                mouseOverJournalist = true;
-                box.style.display = 'block'; //display box
-            };
-
-            box.onmouseleave = function (event) {
-                mouseOverJournalist = false;
-                boxStyle.display = 'none'; //hide box
-
-            };
-        }
-    };
-    for (i = 0; i < journalistQueue.length; i++) {
-        instance.mark(journalistQueue[i].Name, options);
-    }
-}
-
-function getJournalists() {
-    journalistQueue = [];
-    for (var i = 0; i < journalists.length; i++) {
-        if (document.body.innerText.toLowerCase().indexOf(journalists[i].Name.toLowerCase()) >= 0) {
-            journalistQueue.push(journalists[i]);
-        }
-    }
-}
 
 function sendSentences(preferences) {
     //console.log("sendSentences()");
@@ -160,7 +74,6 @@ function sendSentences(preferences) {
             totalWords = document.body.innerText.length;
         }
     }
-    console.log(preferences);
     var body = "";
     if (sentenceQueue.length > 0) {
         body = sentenceQueue.pop(0)
@@ -169,8 +82,6 @@ function sendSentences(preferences) {
                     "topics": preferences};
     var formattedSentences = JSON.stringify(sentences);
 
-    
-    //console.log("about to request");
     var xmlHttp = new XMLHttpRequest();
     xmlHttp.open("POST", "http://localhost:5000/request", false);
     xmlHttp.setRequestHeader("Content-Type", "application/json");
@@ -178,26 +89,12 @@ function sendSentences(preferences) {
     //console.log("we requested");
     return JSON.parse(xmlHttp.responseText);
     
-/*
-    var example = [{
-       "sentence": "Piers Stefan Pughe-Morgan is a British broadcaster,",
-        "results": [{
-            "error": "He did not make an AI",
-            "source": "https://www.politifact.com/factchecks/2020/jul/28/stella-immanuel/dont-fall-video-hydroxychloroquine-not-covid-19-cu/",
-           "correct": "PolitiFact | Hydroxychloroquine is not a COVID-19 cure"
-        }]
-        }];
-
-     return example;
-     */
  }
 
 function updatePage(preferences) {
     console.log("test");
-    var misinformationData = sendSentences(preferences);
-    highlightMisinfo(misinformationData);
-    getJournalists();
-    highlightJournalists();
+    var relevanceData = sendSentences(preferences);
+    highlightRelevant(relevanceData);
     sentenceQueue = [];
     if (original == true) {
         initiateAddedNodes();
@@ -205,10 +102,6 @@ function updatePage(preferences) {
     original = false;
     chrome.runtime.sendMessage({
         type: "pageUpdated"
-    });
-    chrome.runtime.sendMessage({
-        type: "sendPercent",
-        percent: parseInt(100 * (misinfoWords / totalWords), 10)
     });
 }
 
@@ -227,7 +120,7 @@ function createBoxHTML(box, data, index) {
 
     var header = document.createElement('center');
     header.id = "headingTab";
-    header.innerText = "Situation Consensus";
+    header.innerText = "Relevant News";
     box.appendChild(header);
 
     for (var i = 0; i < data[index].results.length; i++) {
@@ -240,31 +133,6 @@ function createBoxHTML(box, data, index) {
     }
 }
 
-function createBoxJournalists(node, box, index) {
-    node.style.backgroundColor = politicalColors[journalistQueue[index].Rating];
-    var header = document.createElement('center');
-    header.id = "headingTab";
-    header.innerText = journalistQueue[index].Name;
-    box.appendChild(header);
-
-    var tab = document.createElement('div');
-    tab.className = "blockTabInfo";
-    var span1 = document.createElement('span');
-    span1.innerText = "This person is considered to have a ";
-    tab.appendChild(span1);
-    var link = document.createElement('a');
-    link.innerText = journalistQueue[index].Rating;
-    link.style.color = politicalColors[journalistQueue[index].Rating];
-    link.style.fontWeight = "bold";
-    link.href = journalistQueue[index].URL;
-    link.target = "_blank";
-    tab.appendChild(link);
-    var span2 = document.createElement('span');
-    span2.innerText = " leaning. " + parseInt(parseFloat(journalistQueue[index].Percent) * 100, 10) + "% of people agree with this rating.";
-    tab.appendChild(span2);
-    box.appendChild(tab);
-}
-
 function initiateAddedNodes() {
     let observer = new MutationObserver((mutations) => {
         mutations.forEach((mutation) => {
@@ -273,7 +141,7 @@ function initiateAddedNodes() {
 
             for (let i = 0; i < mutation.addedNodes.length; i++) {
                 let node = mutation.addedNodes[i]
-                if (node.innerText && node.className != "misinfoBoxes" && node.className != "markUpMisinformation" && node.className != "markUpJournalists") {
+                if (node.innerText && node.className != "newsBoxes" && node.className != "MarkUp") {
                     if (node.innerText.split(" ").length > 7) {
                         sentenceQueue.push(node.innerText);
                         totalWords = totalWords + node.innerText.length;
@@ -293,20 +161,8 @@ function initiateAddedNodes() {
 
 chrome.runtime.onMessage.addListener(
     function (request) {
-        if (request.type == "checkPage")
+        if (request.type == "checkPage") {
             updatePage(request.preferences);
-        else if (request.type == "findMisinformationPercent") {
-            if (totalWords == 0) {
-                chrome.runtime.sendMessage({
-                    type: "sendPercent",
-                    percent: -1
-                });
-            } else {
-                chrome.runtime.sendMessage({
-                    type: "sendPercent",
-                    percent: parseInt(100 * (misinfoWords / totalWords), 10)
-                });
-            }
         }
         else if (request.type == "checkLength") {
             if (document.body.innerText.length > 50000) {
--- flask/src/extension/popUp.js
+++ flask/src/extension/popUp.js
@@ -1,23 +1,9 @@
 var countDown = 0;
 var currentStatus;
-var politicalBias;
-var misinformationPercent;
-var percentAddOn;
 var setup = false;
 var startTimer;
 var button;
 var UNSELECTED = "#9e9e9e";
-var politicalColors = {
-    "Left": "#1565c0",
-    "Left Center": "#42a5f5",
-    "Center": "#ba68c8",
-    "Right Center": "#e57373",
-    "Right": "#e53935",
-    "Fake News": "#ff5722",
-    "Conspiracy": "#ff5722",
-    "Questionable Sources": "#ff5722",
-    "Satire": "#e91e63"
-}
 
 var topics = {"Tech": {"color": "#1976d2"},
             "Finance": {"color": "#4caf50"},
@@ -53,48 +39,6 @@ function updateButtonColor(key) {
     }
     else {
         btn.style.backgroundColor = UNSELECTED;
-    }
-}
-
-function findPoliticalBias() {
-    let params = {
-        active: true,
-        currentWindow: true
-    }
-
-    chrome.tabs.query(params, gotTabs);
-
-    function gotTabs(tabs) {
-        var tab = tabs[0];
-        var url = new URL(tab.url)
-        var domain = url.hostname;
-        console.log(domain);
-        if (sources[domain]) {
-            politicalBias.innerText = sources[domain].bias;
-            politicalBias.href = sources[domain].bias_website;
-            politicalBias.onclick = function () {
-                chrome.tabs.create({ active: true, url: sources[domain].bias_website });
-            };
-            if (politicalColors[sources[domain].bias]) {
-                politicalBias.style.color = politicalColors[sources[domain].bias];
-            }
-        }
-        else {
-            politicalBias.innerText = "None Found";
-        }
-    }
-}
-
-function findPercentMisinfo() {
-    let params = {
-        active: true,
-        currentWindow: true
-    }
-
-    chrome.tabs.query(params, gotTabs);
-
-    function gotTabs(tabs) {
-        chrome.tabs.sendMessage(tabs[0].id, { type: "findMisinformationPercent" });
     }
 }
 
@@ -147,11 +91,9 @@ function timer() {
 }
 
 document.addEventListener('DOMContentLoaded', function () {
-    button = document.getElementById('misinfoScanButton');
+    button = document.getElementById('ScanButton');
     currentStatus = document.getElementById("currentStatus");
-    misinformationPercent = document.getElementById("pageData");
     percentAddOn = document.getElementById("limitWarning");
-    politicalBias = document.getElementById("politicalBias");
     loadButtons();
     let keys = Object.keys(topics);
     chrome.storage.sync.get(keys, function(data) {
@@ -166,7 +108,6 @@ document.addEventListener('DOMContentLoaded', function () {
         }
     });
 
-    findPoliticalBias();
     checkLength();
 });
 
@@ -179,7 +120,6 @@ chrome.runtime.onMessage.addListener(
             }
             if (setup == false) {
                 setup = true;
-                findPercentMisinfo();
                 currentStatus.innerText = "Ready to Scan";
                 currentStatus.style.color = "#43a047";
                 chrome.runtime.sendMessage({ type: "setUp" });
@@ -188,14 +128,6 @@ chrome.runtime.onMessage.addListener(
                 }
             }
 
-        }
-        else if (request.type == "sendPercent") {
-            if (request.percent == -1) {
-                misinformationPercent.innerText = "Must Scan";
-            }
-            else {
-                misinformationPercent.innerText = request.percent + "%";
-            }
         }
         else if (request.type == "setStatus") {
             if (request.status == "Check_Page") {
